3f8acbc246fab0a2dd6d873803bbb25fb97c3d75
Karolina <62151000+nrbtte@users.noreply.github.com>
2023-12-23 23:16:02 +0200

    Implement grid view UI of the meal categories

--- Udemy-Android-Course-Meal-App-main/app/src/main/java/com/example/mealzapp/model/MealCategoriesResponse.kt
+++ Udemy-Android-Course-Meal-App-main/app/src/main/java/com/example/mealzapp/model/MealCategoriesResponse.kt
@@ -13,6 +13,7 @@ data class MealCategoriesResponse(
 data class Category (
     @SerializedName("idCategory")  val id: Int,
     @SerializedName("strCategory")  val name: String,
-    @SerializedName("strCategoryThumb")  val description: String,
-    @SerializedName("strCategoryDescription")  val imageUrl: String
+    @SerializedName("strCategoryDescription")  val description: String,
+    @SerializedName("strCategoryThumb")  val imageUrl: String
+
 )
--- Udemy-Android-Course-Meal-App-main/app/src/main/java/com/example/mealzapp/ui/meals/MealsCategoriesView.kt
+++ Udemy-Android-Course-Meal-App-main/app/src/main/java/com/example/mealzapp/ui/meals/MealsCategoriesView.kt
@@ -1,19 +1,35 @@
 package com.example.mealzapp.ui.meals
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.mealzapp.ui.theme.MealzAppTheme
+import coil.compose.AsyncImage
+import com.example.mealzapp.model.Category
+
 
 @Composable
 fun MealsCategoriesScreen() {
@@ -22,38 +38,75 @@ fun MealsCategoriesScreen() {
 
     Scaffold(
        modifier = Modifier.fillMaxSize(),
-        topBar = { AppBar() }
+        topBar = { AppBar("The MEALZ App") }
     )
     { paddingValues ->
-        LazyColumn(modifier = Modifier.padding(paddingValues)) {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 150.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ){
             items(mealCategories.value) { category ->
-                Text(text = category.name)
+                MealCategoryCard(category){}
 
             }
+
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppBar() {
+fun AppBar(title: String) {
     TopAppBar(
         title = {
-            Text(text = "The MEALZ App")
+            Text(text = title)
         },
         scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
     )
 }
 
-@Composable
-fun MealCategoryIcon(iconUrl: String) {
 
-}
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    MealzAppTheme {
-        MealsCategoriesScreen()
+fun MealCategoryCard(mealCategory: Category, onClickOpenDetails: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .wrapContentHeight(align = Alignment.CenterVertically)
+            .clickable(onClick = { onClickOpenDetails.invoke() }),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp))
+    {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            MealCategoryPicture(mealCategory, 80.dp)
+            Text(
+                text = mealCategory.name,
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
     }
 }
+
+@Composable
+fun MealCategoryPicture(mealCategory: Category, pictureSize: Dp) {
+    Card(
+        modifier = Modifier.padding(16.dp),
+        shape = CircleShape,
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        AsyncImage(
+            model = mealCategory.imageUrl,
+            contentDescription = "Meal ${mealCategory.name} example picture",
+            modifier = Modifier.size(pictureSize),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
